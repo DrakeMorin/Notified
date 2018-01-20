ba70d46fbc7a25388fa4a147755e2525934bef99
Drake Morin <drake.lariviere@gmail.com>
2018-01-19 22:40:32 -0500

    Increase minimum supported version

--- app/src/main/java/com/yeet/notified/MainActivity.kt
+++ app/src/main/java/com/yeet/notified/MainActivity.kt
@@ -2,12 +2,14 @@ package com.yeet.notified
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
     }
 }
