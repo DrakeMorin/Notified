28c03c856a7d08ff7954992fe477b936e4b9d710
Drake Morin <drake.lariviere@gmail.com>
2018-01-19 23:18:41 -0500

    add untested notification listening service

--- app/src/main/java/com/yeet/notified/MainActivity.kt
+++ app/src/main/java/com/yeet/notified/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.yeet.notified
 
-import android.support.v7.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
-import android.widget.TextView
-import org.w3c.dom.Text
+import android.support.v4.content.LocalBroadcastManager
+import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, IntentFilter("Msg"));
 
+    }
+
+    private val onNotice = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val pack = intent.getStringExtra("package")
+            val title = intent.getStringExtra("title")
+            val text = intent.getStringExtra("text")
+
+
+        }
     }
 }
