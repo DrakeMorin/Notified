afd03ae8b795ecb2cc3cde08e1e9ef2820b3ab19
jacobsteves <jgsteves@uwaterloo.ca>
2018-01-20 05:47:45 -0500

    update sql files

--- app/src/main/java/com/yeet/notified/DBHandler.kt
+++ app/src/main/java/com/yeet/notified/DBHandler.kt
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper
 import com.yeet.notified.Models.NotificationReceived
 import com.yeet.notified.Models.NotificationRemoved
 
-private const val DATABASE_VERSION = 1
+private const val DATABASE_VERSION = 2
 private const val DATABASE_NAME = "Notified.db"
 private const val TABLE_NOTIFICATION_RECEIVED = "notification_received"
 private const val COL_ID = "id"
@@ -20,6 +20,7 @@ private const val COL_TITLE = "title"
 private const val COL_TEXT = "text"
 private const val COL_PRIORITY = "priority"
 private const val COL_CATEGORY = "category"
+private const val COL_APP_NAME = "app_name"
 private const val TABLE_NOTIFICATION_REMOVED = "notification_removed"
 private const val COL_REMOVAL_REASON = "removal_reason"
 private const val TABLE_SMS_NOTIFICATION = "sms_notification"
@@ -38,7 +39,8 @@ class DBHandler(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null
                 $COL_TITLE TEXT,
                 $COL_TEXT TEXT,
                 $COL_PRIORITY INTEGER,
-                $COL_CATEGORY TEXT)
+                $COL_CATEGORY TEXT,
+                $COL_APP_NAME TEXT)
                 """
         )
 
@@ -53,6 +55,7 @@ class DBHandler(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null
                 $COL_TEXT TEXT,
                 $COL_PRIORITY INTEGER,
                 $COL_CATEGORY TEXT,
+                $COL_APP_NAME TEXT,
                 $COL_REMOVAL_REASON INTEGER)
                 """
         )
@@ -104,13 +107,14 @@ class DBHandler(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null
     }
 
     // NOTE: Caller MUST close return value
-    private fun getLastNDaysData(days: Int): Cursor {
+    private fun getLastNDaysData(days: Int, appName: String?): Cursor {
         val db = readableDatabase
+        val where = if (appName != null) ", ($COL_APP_NAME) = $appName" else ""
 
         val cursor = db.query(
                 TABLE_NOTIFICATION_RECEIVED,
                 null,
-                "$COL_POST_TIME >= DATEADD(day,-$days, GETDATE())",
+                "$COL_POST_TIME >= DATEADD(day,-$days, GETDATE()) $where",
                 null,
                 null,
                 "$COL_POST_TIME DESC",
@@ -123,21 +127,22 @@ class DBHandler(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null
     }
 
     // NOTE: Caller MUST close return value
-    fun getLastWeekData(): Cursor {
-        return getLastNDaysData(7)
+    fun getLastWeekData(appName: String?): Cursor {
+        return getLastNDaysData(7, appName)
     }
 
     // NOTE: Caller MUST close return value
-    fun getLastDayData(): Cursor {
-        return getLastNDaysData(1)
+    fun getLastDayData(appName: String?): Cursor {
+        return getLastNDaysData(1, appName)
     }
 
     // NOTE: Caller MUST close return value
-    fun getMostFrequent(category: String, isAsc: Boolean): Cursor {
+    fun getMostFrequent(category: String, isAsc: Boolean, appName: String?): Cursor {
         val db = readableDatabase
         val sort = if (isAsc) "ASC" else "DESC"
+        val where = if (appName != null) "WHERE ($COL_APP_NAME) = $appName" else ""
 
-        val rawSql = "SELECT $category, COUNT($category) FROM $TABLE_NOTIFICATION_RECEIVED GROUP BY $category ORDER BY COUNT($category) $sort"
+        val rawSql = "SELECT $category, COUNT($category) FROM $TABLE_NOTIFICATION_RECEIVED $where GROUP BY $category ORDER BY COUNT($category) $sort"
         val cursor = db.rawQuery(rawSql, null)
 
         db.close()
@@ -146,12 +151,13 @@ class DBHandler(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null
     }
 
     // NOTE: Caller MUST close return value
-    fun getMostPopularTime(isDay: Boolean, isAsc: Boolean): Cursor {
+    fun getMostPopularTime(isDay: Boolean, isAsc: Boolean, appName: String?): Cursor {
         val db = readableDatabase
         val sort = if (isAsc) "ASC" else "DESC"
         val day = if (isDay) "DAYNAME($COL_POST_TIME)" else "HOUR($COL_POST_TIME)"
+        val where = if (appName != null) "WHERE ($COL_APP_NAME) = $appName" else ""
 
-        val rawSql = "SELECT $day, COUNT($day) FROM $TABLE_NOTIFICATION_RECEIVED GROUP BY $day ORDER BY COUNT($day) $sort"
+        val rawSql = "SELECT $day, COUNT($day) FROM $TABLE_NOTIFICATION_RECEIVED $where GROUP BY $day ORDER BY COUNT($day) $sort"
         val cursor = db.rawQuery(rawSql, null)
 
         db.close()
