7edb34de4a5f1be2317ef548a12f38599ab48577
Drake Morin <drake.lariviere@gmail.com>
2018-01-20 01:55:26 -0500

    Integrate crashlytics (#2)
    * Integrate Crashlytics
    
    * Remove duplicated line

--- app/src/main/java/com/yeet/notified/MainActivity.kt
+++ app/src/main/java/com/yeet/notified/MainActivity.kt
@@ -7,12 +7,15 @@ import android.content.IntentFilter
 import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
 import android.support.v7.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
         LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, IntentFilter("Msg"));
         DBHandler(this)
